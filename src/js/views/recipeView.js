37603ce21ed12cef16f52818c20431f862430315
Chinmay-G <chinmay.the1@gmail.com>
2025-04-29 09:14:14 +0530

    Fixing the error with icons on browsers

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,6 +1,7 @@
 import View from './View.js';
 
-import icons from 'url:../../img/icons.svg';
+// import icons from 'url:../../img/icons.svg';
+const icons = require('url:../../img/icons.svg');
 import { Fraction } from './fraction.js';
 
 class RecipeView extends View {
